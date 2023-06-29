fdbb5fe0d8f665c2de5afebc61ac74c9fef6066e
Luis_Eduardo <undernet1@gmail.com>
2023-06-29 12:55:59 -0400

    fixed sp

--- StoredProcedures/Orders/dbo.GetRecentOrders.StoredProcedure.sql
+++ StoredProcedures/Orders/dbo.GetRecentOrders.StoredProcedure.sql
@@ -1,18 +1,11 @@
 USE [POS]
 GO
+/****** Object:  StoredProcedure [dbo].[GetRecentOrders]    Script Date: 6/29/2023 12:53:46 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
    CREATE PROCEDURE [dbo].[GetRecentOrders] AS BEGIN
-SELECT
-   CategoryID,
-   CategoryName
-FROM
-   Categories WITH (NOLOCK)
-ORDER BY
-   CategoryName ASC;
-
 SELECT TOP 5 
        [User], 
 	   Products, 

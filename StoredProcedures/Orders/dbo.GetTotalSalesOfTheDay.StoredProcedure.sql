f888ed3a0c00ae8df56683af3e0a02dbf2b0cb2e
Luis_Eduardo <undernet1@gmail.com>
2023-06-30 08:24:07 -0400

    .

--- StoredProcedures/Orders/dbo.GetTotalSalesOfTheDay.StoredProcedure.sql
+++ StoredProcedures/Orders/dbo.GetTotalSalesOfTheDay.StoredProcedure.sql
@@ -1,12 +1,21 @@
 ﻿USE [POS]
 GO
-
+/****** Object: StoredProcedure [dbo].[GetTotalSalesOfTheDay] Script Date: 6/30/2023 8:16:17 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE PROCEDURE [dbo].[GetTotalSalesOfTheDay] AS BEGIN
- SELECT SUM(OrderTotal) AS TotalSalesOfTheDay
-   FROM orders 
- WHERE CONVERT(DATE, OrderDate) = CONVERT(DATE, GETDATE());
+
+CREATE PROCEDURE [dbo].[GetTotalSalesOfTheDay] AS 
+BEGIN
+    DECLARE @TotalSalesOfTheDay DECIMAL(18, 2) = NULL;
+
+    SELECT @TotalSalesOfTheDay = SUM(OrderTotal)
+    FROM orders 
+    WHERE CONVERT(DATE, OrderDate) = CONVERT(DATE, GETDATE());
+
+    IF @TotalSalesOfTheDay IS NOT NULL
+        SELECT @TotalSalesOfTheDay;
+    ELSE
+        SELECT 0 AS TotalSalesOfTheDay;
 END

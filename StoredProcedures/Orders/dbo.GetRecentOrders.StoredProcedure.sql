af618508fb1b69bc66c78b3cbea94a50b1d7bda2
Luis_Eduardo <undernet1@gmail.com>
2023-07-01 09:07:03 -0400

    Added orderID to the returned values

--- StoredProcedures/Orders/dbo.GetRecentOrders.StoredProcedure.sql
+++ StoredProcedures/Orders/dbo.GetRecentOrders.StoredProcedure.sql
@@ -1,13 +1,14 @@
 USE [POS]
 GO
-/****** Object:  StoredProcedure [dbo].[GetRecentOrders]    Script Date: 6/29/2023 12:53:46 PM ******/
+/****** Object:  StoredProcedure [dbo].[GetRecentOrders]    Script Date: 7/1/2023 9:05:54 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
    CREATE PROCEDURE [dbo].[GetRecentOrders] AS BEGIN
 SELECT TOP 5 
-       [User], 
+       [User],
+	   OrderID,
 	   OrderTotal, 
 	   OrderDate 
 FROM Orders WITH (NOLOCK) 

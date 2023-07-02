c9ad3cd6b02ed800e2586b3ea7f09b10b9cab9a9
Luis_Eduardo <undernet1@gmail.com>
2023-07-02 13:06:32 -0400

    Fixed procedure to return data correctly

--- StoredProcedures/Orders/dbo.GetOrderById.StoredProcedure.sql
+++ StoredProcedures/Orders/dbo.GetOrderById.StoredProcedure.sql
@@ -1,32 +1,35 @@
-USE [POS]
-GO
-/****** Object:  StoredProcedure [dbo].[GetOrderById]    Script Date: 7/2/2023 11:11:36 AM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-    ALTER PROCEDURE [dbo].[GetOrderById] 
-	@OrderId UNIQUEIDENTIFIER AS BEGIN
-SELECT
-    Orders.OrderID,
-    Orders.[User],
-	P.ProductName,
-    P.ProductDescription,
-    ProductInfo.ProductQuantity,
-    P.ProductPrice,
-    C.CategoryName AS ProductCategoryName,
-	Orders.OrderSubTotal,
-	CAST(Orders.Discount * Orders.OrderSubTotal AS DECIMAL(18,2)) AS Discount,
-	Orders.OrderTotal,
-	Orders.OrderDate
-FROM
-    Orders WITH (NOLOCK)
-    CROSS APPLY OPENJSON(Orders.Products) WITH (
-        ProductID int,
-        ProductQuantity int
-    ) AS ProductInfo
-    JOIN Products AS P WITH (NOLOCK) ON ProductInfo.ProductID = P.ProductID
-    JOIN Categories AS C WITH (NOLOCK) ON P.ProductCategoryID = C.CategoryID
-WHERE
-    Orders.OrderID = @OrderId;
-END 
+CREATE PROCEDURE [dbo].[GetOrderById] 
+    @OrderId UNIQUEIDENTIFIER
+AS 
+BEGIN
+    SELECT
+        Orders.OrderID,
+        Orders.[User],
+        Orders.OrderSubTotal,
+        CAST(Orders.Discount * Orders.OrderSubTotal AS DECIMAL(18,2)) AS Discount,
+        Orders.OrderTotal,
+        Orders.OrderDate,
+        (
+            SELECT
+                ProductName,
+                ProductDescription,
+                ProductQuantity,
+                ProductPrice,
+                C.CategoryName AS ProductCategoryName
+            FROM
+                Orders
+                CROSS APPLY OPENJSON(Orders.Products) WITH (
+                    ProductID int,
+                    ProductQuantity int
+                ) AS ProductInfo
+                JOIN Products AS P ON ProductInfo.ProductID = P.ProductID
+                JOIN Categories AS C ON P.ProductCategoryID = C.CategoryID
+            WHERE
+                Orders.OrderID = @OrderId
+            FOR JSON PATH
+        ) AS Products
+    FROM
+        Orders
+    WHERE
+        OrderID = @OrderId;
+END

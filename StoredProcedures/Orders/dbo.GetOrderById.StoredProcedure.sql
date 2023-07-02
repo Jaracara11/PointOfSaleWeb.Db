53bf48c9fca4f7f8789b17d9ede553a44f97d648
Luis_Eduardo <undernet1@gmail.com>
2023-07-02 11:49:58 -0400

    working on the getOrderByID procedure

--- StoredProcedures/Orders/dbo.GetOrderById.StoredProcedure.sql
+++ StoredProcedures/Orders/dbo.GetOrderById.StoredProcedure.sql
@@ -1,22 +1,32 @@
 USE [POS]
 GO
-/****** Object:  StoredProcedure [dbo].[GetOrderById]    Script Date: 7/2/2023 9:30:57 AM ******/
+/****** Object:  StoredProcedure [dbo].[GetOrderById]    Script Date: 7/2/2023 11:11:36 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-    CREATE PROCEDURE [dbo].[GetOrderById] 
+    ALTER PROCEDURE [dbo].[GetOrderById] 
 	@OrderId UNIQUEIDENTIFIER AS BEGIN
 SELECT
-    OrderID,
-    [User],
-    Products,
-    OrderSubTotal,
-    Discount,
-    OrderTotal,
-    OrderDate
+    Orders.OrderID,
+    Orders.[User],
+	P.ProductName,
+    P.ProductDescription,
+    ProductInfo.ProductQuantity,
+    P.ProductPrice,
+    C.CategoryName AS ProductCategoryName,
+	Orders.OrderSubTotal,
+	CAST(Orders.Discount * Orders.OrderSubTotal AS DECIMAL(18,2)) AS Discount,
+	Orders.OrderTotal,
+	Orders.OrderDate
 FROM
     Orders WITH (NOLOCK)
+    CROSS APPLY OPENJSON(Orders.Products) WITH (
+        ProductID int,
+        ProductQuantity int
+    ) AS ProductInfo
+    JOIN Products AS P WITH (NOLOCK) ON ProductInfo.ProductID = P.ProductID
+    JOIN Categories AS C WITH (NOLOCK) ON P.ProductCategoryID = C.CategoryID
 WHERE
-    OrderID = @OrderId
+    Orders.OrderID = @OrderId;
 END 

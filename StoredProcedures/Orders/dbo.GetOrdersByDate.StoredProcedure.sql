ac34f56dcbf107c27cbf9d9cfbf6efe522afd6bf
Luis_Eduardo <undernet1@gmail.com>
2023-07-06 13:51:01 -0400

    .

--- StoredProcedures/Orders/dbo.GetOrdersByDate.StoredProcedure.sql
+++ StoredProcedures/Orders/dbo.GetOrdersByDate.StoredProcedure.sql
@@ -1,18 +1,19 @@
 USE [POS]
 GO
-/****** Object:  StoredProcedure [dbo].[GetRecentOrders]    Script Date: 7/5/2023 8:05:50 AM ******/
+/****** Object:  StoredProcedure [dbo].[GetOrdersByDate]    Script Date: 7/6/2023 1:48:22 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-   CREATE PROCEDURE [dbo].[GetOrdersByDate]
+   ALTER PROCEDURE [dbo].[GetOrdersByDate]
    @InitialDate DateTime,
    @FinalDate Datetime
    AS BEGIN
 SELECT 
        OrderID,
        [User],
-	   OrderTotal
+	   OrderTotal,
+	   OrderDate
 FROM Orders WITH (NOLOCK)
 WHERE OrderDate BETWEEN @InitialDate AND @FinalDate
 ORDER BY OrderDate DESC;

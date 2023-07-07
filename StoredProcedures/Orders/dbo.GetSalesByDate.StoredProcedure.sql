3589be64c66581bbdd2b7a8d02061aef65e4b1fa
Luis_Eduardo <undernet1@gmail.com>
2023-07-07 08:11:08 -0400

    .

--- StoredProcedures/Orders/dbo.GetSalesByDate.StoredProcedure.sql
+++ StoredProcedures/Orders/dbo.GetSalesByDate.StoredProcedure.sql
@@ -1,5 +1,6 @@
 USE [POS]
 GO
+/****** Object:  StoredProcedure [dbo].[GetSalesByDate]    Script Date: 7/7/2023 8:06:37 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -9,10 +10,8 @@ CREATE PROCEDURE [dbo].[GetSalesByDate]
 @InitialDate DateTime,
 @FinalDate Datetime
 AS BEGIN
-    SELECT
+ SELECT
         SUM(OrderTotal) AS TotalSales
     FROM Orders WITH (NOLOCK)
-    WHERE OrderDate BETWEEN @InitialDate AND @FinalDate
-    GROUP BY OrderDate
-    ORDER BY OrderDate;
+    WHERE OrderDate BETWEEN @InitialDate AND @FinalDate;
 END

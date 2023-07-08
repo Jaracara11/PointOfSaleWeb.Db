a36aaa98cfe048110574d9bcde4bc798205fa7a7
Luis_Eduardo <undernet1@gmail.com>
2023-07-08 09:00:40 -0400

    Added date format

--- StoredProcedures/Orders/dbo.GetOrdersByDate.StoredProcedure.sql
+++ StoredProcedures/Orders/dbo.GetOrdersByDate.StoredProcedure.sql
@@ -1,20 +1,24 @@
 USE [POS]
 GO
-/****** Object:  StoredProcedure [dbo].[GetOrdersByDate]    Script Date: 7/6/2023 1:48:22 PM ******/
+/****** Object:  StoredProcedure [dbo].[GetOrdersByDate]    Script Date: 7/8/2023 8:54:50 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-   ALTER PROCEDURE [dbo].[GetOrdersByDate]
-   @InitialDate DateTime,
-   @FinalDate Datetime
-   AS BEGIN
-SELECT 
-       OrderID,
-       [User],
-	   OrderTotal,
-	   OrderDate
-FROM Orders WITH (NOLOCK)
-WHERE OrderDate BETWEEN @InitialDate AND @FinalDate
-ORDER BY OrderDate DESC;
+
+CREATE PROCEDURE [dbo].[GetOrdersByDate]
+@InitialDate DateTime,
+@FinalDate Datetime
+AS BEGIN
+    SET @InitialDate = DATEADD(DAY, DATEDIFF(DAY, 0, @InitialDate), 0)
+    SET @FinalDate = DATEADD(MINUTE, -1, DATEADD(DAY, DATEDIFF(DAY, 0, @FinalDate) + 1, 0))
+
+    SELECT 
+        OrderID,
+        [User],
+        OrderTotal,
+        OrderDate
+    FROM Orders WITH (NOLOCK)
+    WHERE OrderDate BETWEEN @InitialDate AND @FinalDate
+    ORDER BY OrderDate DESC;
 END
--- StoredProcedures/Orders/dbo.GetSalesByDate.StoredProcedure.sql
+++ StoredProcedures/Orders/dbo.GetSalesByDate.StoredProcedure.sql
@@ -1,15 +1,18 @@
 USE [POS]
 GO
-/****** Object:  StoredProcedure [dbo].[GetSalesByDate]    Script Date: 7/7/2023 8:06:37 AM ******/
+/****** Object:  StoredProcedure [dbo].[GetSalesByDate]    Script Date: 7/8/2023 8:55:43 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-ALTER PROCEDURE [dbo].[GetSalesByDate]
+CREATE PROCEDURE [dbo].[GetSalesByDate]
 @InitialDate DateTime,
 @FinalDate Datetime
 AS BEGIN
+    SET @InitialDate = DATEADD(DAY, DATEDIFF(DAY, 0, @InitialDate), 0)
+    SET @FinalDate = DATEADD(MINUTE, -1, DATEADD(DAY, DATEDIFF(DAY, 0, @FinalDate) + 1, 0))
+
 	DECLARE @TotalSales decimal(18, 2);
 
 	SELECT @TotalSales = SUM(OrderTotal)

54e26a26805ded9bb1cb2adde1c03a48cea5de92
Luis_Eduardo <undernet1@gmail.com>
2024-08-26 08:25:16 -0400

    Updated getAllProducts sp to include category info

--- StoredProcedures/Products/dbo.GetAllProducts.StoredProcedure.sql
+++ StoredProcedures/Products/dbo.GetAllProducts.StoredProcedure.sql
@@ -1,23 +1,27 @@
 USE [POS]
 GO
-/****** Object:  StoredProcedure [dbo].[GetAllProducts]    Script Date: 5/25/2023 4:59:40 PM ******/
+/****** Object:  StoredProcedure [dbo].[GetAllProducts]    Script Date: 8/26/2024 8:24:22 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+
 CREATE PROCEDURE [dbo].[GetAllProducts]
 AS
 BEGIN
    SELECT
-   ProductID,
-   ProductName,
-   ProductDescription,
-   ProductStock,
-   ProductCost,
-   ProductPrice,
-   ProductCategoryID
-FROM
-   Products WITH (NOLOCK)
-ORDER BY
-   ProductName ASC;
+       p.ProductID,
+       p.ProductName,
+       p.ProductDescription,
+       p.ProductStock,
+       p.ProductCost,
+       p.ProductPrice,
+       c.CategoryID,
+       c.CategoryName
+   FROM
+       Products p WITH (NOLOCK)
+   INNER JOIN
+       Categories c WITH (NOLOCK) ON p.ProductCategoryID = c.CategoryID
+   ORDER BY
+       p.ProductName ASC;
 END

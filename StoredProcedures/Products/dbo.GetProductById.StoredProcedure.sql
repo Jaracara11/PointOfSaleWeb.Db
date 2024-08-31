2169348a05a97a93cdac916a2a9d9c734a059411
Luis_Eduardo <undernet1@gmail.com>
2024-08-31 11:45:45 -0400

    Updated getProduct object

--- StoredProcedures/Products/dbo.GetProductById.StoredProcedure.sql
+++ StoredProcedures/Products/dbo.GetProductById.StoredProcedure.sql
@@ -1,26 +1,25 @@
 USE [POS]
 GO
-    /****** Object:  StoredProcedure [dbo].[GetProductById]    Script Date: 4/8/2023 5:21:54 PM ******/
-SET
-    ANSI_NULLS ON
+/****** Object:  StoredProcedure [dbo].[GetProductById]    Script Date: 8/31/2024 11:41:22 AM ******/
+SET ANSI_NULLS ON
 GO
-SET
-    QUOTED_IDENTIFIER ON
+SET QUOTED_IDENTIFIER ON
 GO
     CREATE PROCEDURE [dbo].[GetProductById] 
     @ProductId NVARCHAR(50) 
     AS BEGIN
 SELECT
-    ProductID,
-    ProductName,
-    ProductDescription,
-    ProductStock,
-    ProductCost,
-    ProductPrice,
-    ProductCategoryID
-FROM
-    Products WITH (NOLOCK)
-WHERE
-    ProductID = @ProductId
+       p.ProductID,
+       p.ProductName,
+       p.ProductDescription,
+       p.ProductStock,
+       p.ProductCost,
+       p.ProductPrice,
+       c.CategoryID,
+	   c.CategoryName
+   FROM
+       Products p WITH (NOLOCK)
+   INNER JOIN
+       Categories c WITH (NOLOCK) ON p.ProductCategoryID = c.CategoryID
+   WHERE ProductID = @ProductId
 END
-GO
